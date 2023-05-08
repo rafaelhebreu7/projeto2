8b86aade7d5042ede63c362296e4969702d83f1e
rafaelhebreu7 <estudojavagithub@gmail.com>
2023-05-08 14:00:41 -0300

    Criação projeto 01

---
+++ projetojava01/src/com/teste/github/Primeiraclasse.java
@@ -0,0 +1,14 @@
+package com.teste.github;
+
+public class Primeiraclasse {
+
+	public Primeiraclasse() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
